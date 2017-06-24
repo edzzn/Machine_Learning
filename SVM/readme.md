dc6c184bf9db140345371c093b029af16a716755
Edisson Reinozo <edisson350@gmail.com>
2017-06-24 17:57:12 -0500

    SVM and SVC examples

---
+++ SVM/readme.md
@@ -0,0 +1,18 @@
+# SVM - Support Vector Machine
+
+## What is Support Vector Machine?
+SVM is a supervised machine learning algorithm which can be used for classification and regression challenges. It is mostly used for classification.
+ 
+In this algorithm, we plot each data item as a point in n-dimensional space with the value of each feature being the value of a particular coordinate. Then we perform a classification by finding the hyper-plane that differentiate the two classes.
+
+Support Vectors are the coordinates of individual observations. 
+## How does it work?
+SVM selects the hyper-plane which classifies the classes accurately prior to maximizing the margin.
+
+
+
+### Resources:
+["Understanding Support Vector Machine algorithm"](https://www.analyticsvidhya.com/blog/2015/10/understaing-support-vector-machine-example-code/)
+
+["Support Vector Machine – Simplified"](https://www.analyticsvidhya.com/blog/2014/10/support-vector-machine-simplified/)
+
---
+++ SVM/svm_svc_rbf_Kernel.py
@@ -0,0 +1,39 @@
+# SVC with rbf Kernel
+# using the iris data set
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
+
+# import data
+iris = datasets.load_iris()
+X = iris.data[:, :2] # we'll only  use 2 features
+
+y = iris.target
+
+# Create an instance of SVM and fit our data.
+# We do not scale our data since we want to plot the support vectors
+# we remain in a 2D view
+
+C = 1 # SVM regularization parameter
+svc = svm.SVC(kernel='rbf', C=0.02, gamma=0.01).fit(X, y)
+
+# create a mesh to plot in
+if __name__ == '__main__':
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+h = (x_max / x_min)/100
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+plt.subplot(1, 1, 1)
+Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
+plt.xlabel('Sepal lenght')
+plt.ylabel('Sepal width')
+plt.xlim(xx.min(), xx.max())
+plt.title('SVC with rbf Kernel')
+
+plt.show()
